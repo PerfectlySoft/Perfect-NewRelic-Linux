f28cbdb75a1caa85c0cb32832fe8bb95ae7e0b2a
Rockford Wei <rocky@perfect.org>
2017-04-26 09:53:21 -0400

    Adding curl & openssl installation script

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
-cd /usr/local && wget http://download.newrelic.com/agent_sdk/nr_agent_sdk-v0.16.2.0-beta.x86_64.tar.gz -O /tmp/nr.tgz && sudo tar --strip-components=1 -zxvf /tmp/nr.tgz && sudo ldconfig
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install libcurl4-openssl-dev
+cd /usr/local
+wget http://download.newrelic.com/agent_sdk/nr_agent_sdk-v0.16.2.0-beta.x86_64.tar.gz -O /tmp/nr.tgz
+sudo tar --strip-components=1 -zxvf /tmp/nr.tgz
+udo ldconfig
